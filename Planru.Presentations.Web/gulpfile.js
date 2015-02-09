0c3c101a2b4c095f4c5c2f4a95c394ffbc89fc9b
Liep Nguyen <liepnguyen.it@gmail.com>
2015-02-10 01:36:58 +0700

    Refactor code

--- Planru.Presentations.Web/gulpfile.js
+++ Planru.Presentations.Web/gulpfile.js
@@ -2,15 +2,48 @@
 var uglify = require('gulp-uglify');
 var less = require('gulp-less');
 var concat = require('gulp-concat');
+var bowerFiles = require('main-bower-files');
+var del = require('del');
+var yargs = require('yargs').argv;
+var minifyHTML = require('gulp-minify-html');
 
-gulp.task('default', function () {
-    
+var paths = {
+	scripts: ['src/**/*.js'],
+	images: ''
+};
+
+gulp.task('clean', function(cb) {
+  del(['build'], cb);
 });
 
-gulp.task('minifyJs', function () {
-    gulp.src('src/**/.js')
-  		.pipe(uglify({
-  		    outSourceMap: true
-  		}))
-  		.pipe(gulp.dest('dist'));
+gulp.task('build:js', function () {
+    if (yargs.release) {
+    	gulp.src(paths.scripts)
+    		//.pipe(concat('main.min.js'))
+    		.pipe(uglify())
+    		.pipe(gulp.dest('build'));
+    }
+    else {
+    	gulp.src(paths.scripts)
+    		//.pipe(concat('main.min.js'))
+    		.pipe(gulp.dest('build'));
+    }
 });
+
+gulp.task('build:html', function() {
+    if (yargs.release) {
+        gulp.src(['src/**/*.html'])
+            .pipe(minifyHTML({ comments: false, spare: false }))
+            .pipe(gulp.dest('build'));
+    }
+    else {
+
+    }
+});
+
+gulp.task('build:bower', function () {
+    gulp.src(bowerFiles({}), { base: 'bower_components' })
+		.pipe(gulp.dest('build/lib'));
+});
+
+gulp.task('default', ['build:js', 'build:html', 'build:bower']);
