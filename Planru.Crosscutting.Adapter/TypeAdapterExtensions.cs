f5d7abf7397730ce4abf4c6ab6b2cc2deedcb8fd
Liep Nguyen <liepnguyen@kms-technology.com>
2015-02-27 15:46:14 +0700

    Refactor code

---
+++ Planru.Crosscutting.Adapter/TypeAdapterExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Planru.Crosscutting.Adapter
+{
+    public static class TypeAdapterExtensions
+    {
+        public static T AdaptTo<T>(this object source)
+        {
+            return (T)new object();
+        }
+    }
+}
