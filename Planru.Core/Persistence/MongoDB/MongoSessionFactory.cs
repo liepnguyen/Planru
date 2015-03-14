df1671a4b959885166368f988810c9d62ec5f5eb
Liep Nguyen <liepnguyen.it@gmail.com>
2015-03-14 16:19:06 +0700

    Refactor code

---
+++ Planru.Core/Persistence/MongoDB/MongoSessionFactory.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+
+namespace Planru.Core.Persistence.MongoDB
+{
+    public class MongoSessionFactory : IMongoSessionFactory
+    {
+        /// <summary>
+        /// Gets the connection string
+        /// </summary>
+        public string ConnectionString { get; private set; }
+
+        public MongoSessionFactory(string connectionString)
+        {
+            if (string.IsNullOrEmpty(connectionString))
+                throw new ArgumentNullException("connectionString");
+
+            this.ConnectionString = connectionString;
+        }
+
+        public MongoDatabase OpenConnection(string databaseName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
