8ce60bfe667bf3e23f7249def19ba9d4172d0d8a
Liep Nguyen <liepnguyen.it@gmail.com>
2015-04-06 02:11:43 +0700

    Refactor code

--- Planru.Crosscutting.Adapter/IMemberConfiguration.cs
+++ Planru.Crosscutting.Adapter/IMemberConfiguration.cs
@@ -9,7 +9,15 @@ namespace Planru.Crosscutting.Adapter
 {
     public interface IMemberConfiguration<TSource, TTarget>
     {
+        /// <summary>
+        /// Specify the source member to map from
+        /// </summary>
+        /// <param name="sourceMember"></param>
         void MapFrom(Expression<Func<TSource, object>> sourceMember);
+
+        /// <summary>
+        /// Ignore this member for configuration validation and skip during mapping
+        /// </summary>
         void Ignore();
     }
 }
